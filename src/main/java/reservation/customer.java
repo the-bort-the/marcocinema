9bb83e22b172cbfe8e48dafbac5c68024dc87739
jan6673 <michaeljandora@northwesternmutual.com>
2021-11-04 11:51:38 -0500

    Initial commit

---
+++ src/main/java/reservation/customer.java
@@ -0,0 +1,9 @@
+package reservation;
+
+public class customer {
+    public static void main(String[] args) {
+        System.out.println("Hello MarcoCinema!!");
+    }
+
+    //composite value type composed of other value Types, not a simple one
+}
---
+++ src/test/java/reservation/customerTest.java
@@ -0,0 +1,12 @@
+package reservation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class customerTest {
+@Test
+    public void customerCreate() {
+    assertTrue(true);
+}
+}
